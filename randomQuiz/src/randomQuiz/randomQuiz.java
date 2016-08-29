80eda71b34daac71cacd16eacd1fdda98752fee1
AndrewOrdonez <andrewordonez5@gmail.com>
2016-08-29 09:58:05 -0600

    initial commit

---
+++ randomQuiz/src/randomQuiz/randomQuiz.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class randomQuiz {
+static Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("how many questions would you like to answer?");
+int questions = scan.nextInt();
+String apple = "";
+for (int i = 0; i < questions;i++){
+	System.out.println(i);
+}
+
+	}
+
+}
