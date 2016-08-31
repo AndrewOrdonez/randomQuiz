e1f8413ee9edd46cd5fb08071f376361f1706008
AndrewOrdonez <andrewordonez5@gmail.com>
2016-08-30 20:56:14 -0600

    finished product

--- randomQuiz/src/randomQuiz/randomQuiz.java
+++ randomQuiz/src/randomQuiz/randomQuiz.java
@@ -10,14 +10,14 @@ int questions = scan.nextInt();
 String apple = "";
 int correct =0;
 for (int i = 0; i < questions;i++){
-	double pear =Math.random()*3;
+	double pear =Math.random()*2;
 	int first = (int)(Math.random()*10);
 	int numberCorrect =0;
 	int last = (int)(Math.random()*10)+first;
 	String question1 ="what is the lowest possible number that '(int)(Math.random()*"+last+")+"+first+";' would generate";
 	String question2 ="what is the highest possible number that '(int)(Math.random()*"+last+")+"+first+";' would generate";
-	String question3 ="please enter the code for returning a psuedo-random number between "+first+" and "+(first+last-1)+".";
-if (pear<=1){
+	String question3 ="please enter the code for returning a psuedo-random number between "+first+" and "+(first+last-1)+" inclusive.";
+if (pear<1){
 	System.out.println(question1);
 	int answer1 =scan.nextInt();
 	if (answer1 == first){
@@ -42,12 +42,12 @@ else if (pear<2&&pear>1){
 else {
 	System.out.println(question3);
 	String answer3 =scan.next();
-	if (answer3.equals("(int)(Math.random()*"+last+")+"+first+")" )){
+	if (answer3.equals("(int)(Math.random()*"+last+")+"+first )){
 		System.out.println("correct!");
 		correct++;
 	}
 	else {
-		System.out.println("incorrect, the corect answer is: "+"(int)(Math.random()*"+last+")+"+first+")");
+		System.out.println("incorrect, the corect answer is: "+"(int)(Math.random()*"+last+")+"+first);
 	}
 }
 
